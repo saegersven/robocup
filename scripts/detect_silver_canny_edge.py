46303a5f19b3cefeaa3479d90db0b22481ff82d4
Lupus3141 <lukas.kraemer.2004@gmail.com>
2021-08-19 18:16:06 +0200

    some testing for silver detection using canny edge

--- scripts/detect_silver_canny_edge.py
+++ scripts/detect_silver_canny_edge.py
@@ -1,1 +1,20 @@
 import cv2
+import random
+
+image = cv2.imread("./images/silver_test_images/2.jpg")
+
+resized = cv2.resize(image, (400, 240))
+
+while 1:
+	# highly effictive stuff going on here:
+	p1 = random.randint(0, 300)
+	p2 = random.randint(0, 300)
+
+	edges = cv2.Canny(image, p1, p2)
+	edges = cv2.resize(edges, (400, 240)) # display image a bit bigger
+
+	cv2.imshow("Original", resized)
+	cv2.imshow("Canny Edge", edges)
+
+	print(f"p1: {p1}, p2: {p2}")
+	cv2.waitKey(0)
