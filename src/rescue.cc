2d1d631a86fdc8ce58cc3f904dc0e789724543c1
Lupus3141 <57531901+Lupus3141@users.noreply.github.com>
2021-11-28 18:15:53 +0100

    doc in rescue.cc

--- src/rescue.cc
+++ src/rescue.cc
@@ -1,16 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+#include <opencv2/opencv.hpp>
+
+#include "robot.h"
+#include "rescue.h"
+#include "utils.h"
+
 // drives to victim and picks it up
 void rescueVictim() {
 
 }
 
-// returns reflectance of victim to distinguish between "living" and "dead"
+/**
+ * returns reflectance of victim to distinguish between "living" and "dead"
+ *
+ * @param current camera frame
+ * @return reflectance of victim in image (low reflectance -> dead victim)
+ *
+*/
 int victimReflectance(cv::Mat& frame) {
 	return 42;
 }
 
-//checks if there are round contours in the given frame
+/**
+ * checks if there are round contours (-> victim) in the given frame
+ *
+ * @param current camera frame
+ * @return boolean value
+ *
+*/
 bool victimInFrame(cv::Mat& frame) {
-	return false;
+	cv::GaussianBlur(frame, image, Size(5, 5), 1.0); // adjust params
+	
+	cv::cvtColor(image, cv::Mat gray, cv2::COLOR_BGR2GRAY);
+
+	vector<Vec3f> circles;
+    HoughCircles(gray, circles, HOUGH_GRADIENT, 2, gray.rows/4, 200, 100); // old Python parameters: minDist = 60, param1 = 34, param2 = 24, minRadius = 2, maxRadius = 300
+
+	if(circles.size() != 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // searches the corner and drops victim
