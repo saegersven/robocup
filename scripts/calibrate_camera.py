e0ea6397f99ef91a9624f125cf9f8154e2ee0375
Lupus3141 <57531901+Lupus3141@users.noreply.github.com>
2021-12-27 14:24:09 +0100

    kjserbgfjkhsrbegv

--- scripts/calibrate_camera.py
+++ scripts/calibrate_camera.py
@@ -3,15 +3,18 @@ import cv2
 import glob
 import array
 import time
+import json
 
-image_folder = str(input("Input image folder: "))
-out_file = str(input("Input output file: "))
+image_folder = "img" # str(input("Input image folder: "))
+out_file = "test.json" # str(input("Input output file: "))
 
+X = 5
+Y = 7
 # Termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+objp = np.zeros((X*Y, 3), np.float32)
+objp[:, :2] = np.mgrid[0:X, 0:Y].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -25,15 +28,18 @@ for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     g = gray
+    cv2.imshow("g", g)
+    cv2.waitKey(500)
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (X,Y), None)
     # If found, add object points, image points (after refining them)
     if ret:
+        print("Found")
         objpoints.append(objp)
         corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv2.drawChessboardCorners(img, (X,Y), corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(500)
 
@@ -45,10 +51,18 @@ print("Calibrating")
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints,
     g.shape[::-1], None, None)
 
-output_file = open(out_file, "wb")
-float_array = array.array('f', [mtx[0][0], mtx[1][1], mtx[0, 2], mtx[2, 1],
-    dist[0][0], dist[0][1], dist[0][2], dist[0][3], dist[0][4]])
-float_array.tofile(output_file)
+out = {
+    'fx': mtx[0][0],
+    'fy': mtx[1][1],
+    'cx': mtx[0][2],
+    'cy': mtx[2][1],
+
+    'k': [dist[0][0], dist[0][1], dist[0][2], dist[0][3], dist[0][4]]
+}
+j = json.dumps(out, indent=4)
+
+output_file = open(out_file, 'w')
+output_file.write(j)
 output_file.close()
 
 print("Written camera parameters to output file")
--- scripts/calibration_capture.py
+++ scripts/calibration_capture.py
@@ -8,15 +8,15 @@ cam = cv2.VideoCapture(cam_id)
 counter = 0
 
 while True:
-	cmd = str(input("Press enter to capture, type anything to abort: "))
-	if(len(cmd) != 0):
-		break
 	ret, img = cam.read()
 	cv2.imshow("Captured image", img)
-	cv2.imwrite(folder + "/" + str(counter) + ".jpg", img)
 
-	counter = counter + 1
-	if cv2.waitKey(1) == 27:
+	w = cv2.waitKey(1)
+	if w == ord('c'):
+		cv2.imwrite(folder + "/" + str(counter) + ".jpg", img)
+		print("Captured")
+		counter = counter + 1
+	elif w == ord('q'):
 		break
 
 cv2.destroyAllWindows()
