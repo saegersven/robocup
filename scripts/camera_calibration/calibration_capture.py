35de400061ae0ad3c7dfbf3655f1a5d2146de018
Lupus3141 <lukas.kraemer.2004@gmail.com>
2022-03-27 18:22:24 +0200

    added ml data for silver

--- scripts/camera_calibration/calibration_capture.py
+++ scripts/camera_calibration/calibration_capture.py
@@ -1,7 +1,7 @@
 import cv2
 
 cam_id = int(input("Input camera Id: "))
-# folder = str(input("Input output folder: "))
+folder = str(input("Input output folder: "))
 
 cam = cv2.VideoCapture(cam_id, cv2.CAP_V4L)
 
@@ -9,11 +9,12 @@ counter = 0
 
 while True:
 	ret, img = cam.read()
+	img = cv2.flip(img, -1)
 	cv2.imshow("Captured image", img)
 
 	w = cv2.waitKey(1)
 	if w == ord('c'):
-		# cv2.imwrite(folder + "/" + str(counter) + ".jpg", img)
+		cv2.imwrite(folder + "/" + str(counter) + ".jpg", img)
 		print("Captured")
 		counter = counter + 1
 	elif w == ord('q'):
