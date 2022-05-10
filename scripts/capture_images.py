64206400c60fa4affed833bcea3347913fd10d63
Lupus3141 <lukas.kraemer.2004@gmail.com>
2022-05-10 18:51:10 +0200

    added further victim ml data

--- scripts/capture_images.py
+++ scripts/capture_images.py
@@ -2,14 +2,15 @@ import cv2
 import time
 
 cap = cv2.VideoCapture(0)
-
+img_cnt = 0
 while(True):
 	_, frame = cap.read()
 	cv2.imshow('frame', frame)
 	if cv2.waitKey(1) & 0xFF == ord('c'):
 		path = "/home/pi/Desktop/imgs/" + str(time.time()) + ".png"
 		cv2.imwrite(path, frame)
-		print("saved image")
+		print("saved image", img_cnt)
+		img_cnt = img_cnt + 1
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
